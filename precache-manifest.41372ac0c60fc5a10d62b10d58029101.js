bd240c26640f307188c62315b61878569c2c0c6f
DoDatVu <datvu22@gmail.com>
2019-04-30 23:58:25 -0500

    Updates

---
+++ precache-manifest.41372ac0c60fc5a10d62b10d58029101.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63828004b707048d4ce2104205bbb578",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d5d0f19a4f1d8db6501a",
+    "url": "/clicky-game/static/css/main.3f093ec0.chunk.css"
+  },
+  {
+    "revision": "a14e975f0489b6558936",
+    "url": "/clicky-game/static/js/2.797b3c14.chunk.js"
+  },
+  {
+    "revision": "d5d0f19a4f1d8db6501a",
+    "url": "/clicky-game/static/js/main.33763c87.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "1d47b4fcd393a145f7d0278e576cb73d",
+    "url": "/clicky-game/static/media/booseungkwan.1d47b4fc.jpg"
+  },
+  {
+    "revision": "89a6447703c76e0f1bfc6b5b2cb52a42",
+    "url": "/clicky-game/static/media/dino.89a64477.jpg"
+  },
+  {
+    "revision": "9795fcacc16c8d13493b1534d0dcef4b",
+    "url": "/clicky-game/static/media/dk.9795fcac.jpg"
+  },
+  {
+    "revision": "45cfbdc83ba0beac7e7ba2fed4f7ebf6",
+    "url": "/clicky-game/static/media/jeongyeon.45cfbdc8.jpg"
+  },
+  {
+    "revision": "8f00e0362d04ee4f1a2d0d009f06efd2",
+    "url": "/clicky-game/static/media/joshua.8f00e036.jpg"
+  },
+  {
+    "revision": "954f70937c6954cbe494c759f134b8ce",
+    "url": "/clicky-game/static/media/kimmingyu.954f7093.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fec258e9e27a6c5820b320babc06fea8",
+    "url": "/clicky-game/static/media/scoups.fec258e9.jpg"
+  },
+  {
+    "revision": "58e264240b3ab7885df65f1b3ad13e71",
+    "url": "/clicky-game/static/media/terrycrews.58e26424.jpg"
+  },
+  {
+    "revision": "05fad55ad183de9b5395ffc5af84cdb0",
+    "url": "/clicky-game/static/media/vernon.05fad55a.jpeg"
+  },
+  {
+    "revision": "cdbcf7061acdc2fb05c0a74d36192c1a",
+    "url": "/clicky-game/static/media/wonwoo.cdbcf706.jpg"
+  },
+  {
+    "revision": "3f596a619383cc89558fc28c88348fbb",
+    "url": "/clicky-game/static/media/woozi.3f596a61.jpeg"
+  },
+  {
+    "revision": "ddb0b0d1e2f6253bc1252f4b3d53264f",
+    "url": "/clicky-game/static/media/xuminghao.ddb0b0d1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.c31d78d9b8e28d2d8f5b09986eeabc03.js"
+  "/clicky-game/precache-manifest.41372ac0c60fc5a10d62b10d58029101.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
